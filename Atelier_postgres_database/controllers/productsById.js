be9a9072be3d8ffe4eb1722558e5223083c21150
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-16 20:48:36 -0400

    Implement get method
    get method will invoke the productsModel getProductsById method on the
    id parameter provided in the request.
    
    get method will send the results of a succesful operation to the client
    
    get method will send an error to the client if the request fails

--- Atelier_postgres_database/controllers/productsById.js
+++ Atelier_postgres_database/controllers/productsById.js
@@ -2,6 +2,15 @@ const productsModel = require('../models/index.js').productsModel;
 
 module.exports = {
   get: function (req, res) {
-    res.send('products by id endpoint')
+    var id = req.params.product_id
+
+    productsModel.getProductsById(id)
+      .then((result) => {
+        res.status(200).send(result)
+      })
+      .catch((err) => {
+        console.log('error sending that product to client', err);
+        res.status(404).send('Could not find that product');
+      })
   }
 }
