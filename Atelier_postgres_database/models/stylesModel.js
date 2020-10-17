6fd6defa98a4bda81997aa024d4861c9d8a49e5d
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-16 22:25:09 -0400

    Implement getStyles method
    This method will get all style entries associated with the given
    productId.
    
    It will store those as a result array on an object that trakcs the
    product id.
    
    It will then use the style ids it finds to find all photo entries and
    sku entries associated with each of the styles found in the original db
    call.
    
    The results of that second round of queries will be appended into their
    respective styles before return the compiled resultObj to the controller

--- Atelier_postgres_database/models/stylesModel.js
+++ Atelier_postgres_database/models/stylesModel.js
@@ -1,7 +1,64 @@
 const Style = require('../database/index.js').Style;
+const Photo = require('../database/index.js').Photo;
+const Sku = require('../database/index.js').Sku;
 
 module.exports = {
-  getStyles: function () {
+  getStyles: function (id) {
+  var resultObj = {product_id: id}
+  var styleIds = []
+  return Style.findAll({
+      attributes: {
+        exclude: ['product_id']
+      },
+      where: {
+        product_id: id
+      }
+    })
+    .then((styles) => {
+      var styleResults = [];
+      styles.forEach((style) => {
+        style.dataValues.photos = [];
+        style.dataValues.skus = {};
+        styleResults.push(style.dataValues)
+        styleIds.push(style.dataValues.style_id)
+      })
+      resultObj.results = styleResults
+      var stylePhotoSets = styleIds.map((currId) => {
+        return Photo.findAll({
+          attributes: ['url', 'thumbnail_url'],
+          where: {
+            style_id: currId
+          }
+        })
+      })
 
+      var styleSkuSets = styleIds.map((currId) => {
+        return Sku.findAll({
+          attributes: ['size', 'quantity'],
+          where: {
+            style_id: currId
+          }
+        })
+      })
+
+    return Promise.all([...stylePhotoSets,...styleSkuSets])
+    })
+    .then((resultSet) => {
+      var pi,si;
+      for (pi = 0, si = styleIds.length; pi < styleIds.length; pi++,si++) {
+        console.log(`entry ${pi}`)
+        resultSet[pi].forEach((entry => {
+          resultObj.results[pi].photos.push(entry.dataValues);
+        }));
+        resultSet[si].forEach((entry => {
+          resultObj.results[pi].skus[entry.dataValues.size] = entry.dataValues.quantity
+        }))
+      }
+      console.log(resultObj)
+      return resultObj;
+    })
+    .catch((err) => {
+      console.log('error finding that product in db', err);
+    })
   }
 }
