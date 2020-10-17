9ced71acfd5ac997b9c09a8c51d6744b34978dc3
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-16 22:24:35 -0400

    Change names of controllers to be more semantic

--- Atelier_postgres_database/routes.js
+++ Atelier_postgres_database/routes.js
@@ -3,11 +3,11 @@ var router = require('express').Router();
 
 router.get('/products', controller.products.get);
 
-router.get('/products/:product_id', controller.productsById.get);
+router.get('/products/:product_id', controller.productById.get);
 
-router.get('/products/:product_id/styles', controller.productsStyles.get);
+router.get('/products/:product_id/styles', controller.productStyles.get);
 
-router.get('/products/:product_id/related', controller.productsRelated.get);
+router.get('/products/:product_id/related', controller.productRelated.get);
 
 router.get('/cart', controller.cart.get);
 
