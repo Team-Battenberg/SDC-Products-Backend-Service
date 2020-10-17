d770ab010bd320dd34fa6ae06e9c73eba2ba6d72
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-16 22:28:20 -0400

    Change name of controllers to be more semantic

--- Atelier_postgres_database/controllers/productsStyles.js
+++
@@ -1,8 +0,0 @@
-const stylesModel = require('../models/index.js').stylesModel;
-const skusModel = require('../models/index.js').skusModel;
-
-module.exports = {
-  get: function (req, res) {
-    res.send('product styles endpoint')
-  }
-}
