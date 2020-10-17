b18808db4c5119c516fc5f1060c8e3f1e918bc20
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-16 22:36:23 -0400

    Implement get method
    get method will pass the id provided in request params to
    relatedModel.getRelated method for handling in model.
    
    Upon success will send a 200 response to client along with the resulting
    data
    
    Upon failure will console log the error and send a 404 to the client

--- Atelier_postgres_database/controllers/productRelated.js
+++ Atelier_postgres_database/controllers/productRelated.js
@@ -2,6 +2,14 @@ const relatedModel = require('../models/index.js').relatedModel;
 
 module.exports = {
   get: function (req, res) {
-    res.send('products related endpoint')
+    var id = req.params.product_id
+    relatedModel.getRelated(id)
+      .then((result) => {
+        res.status(200).send(result)
+      })
+      .catch((err) => {
+        console.log('error sending related products to client', err);
+        res.status(404).send('Could not find related products');
+      })
   }
 }
