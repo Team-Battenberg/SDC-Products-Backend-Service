3dea61ca9fdbe1bd321049a43df4a651c73a8c9e
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-23 11:38:01 -0400

    Adjust pool connection settings

--- Atelier_postgres_database/database/index.js
+++ Atelier_postgres_database/database/index.js
@@ -1,11 +1,15 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-});
+  max: 30,
+  idleTimeoutMillis: 10000,
+})
+
 pool.connect()
   .then(() => console.log(`Connected to pg on port ${process.env.PGPORT}`))
-  .catch(err => console.error('connection error', err.stack))
-module.exports.pool = pool
+  .catch(err => console.error('connection error', err.stack));
+
+module.exports.pool = pool;
